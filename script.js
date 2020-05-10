cab9deab034d8bce5ecc2f96b66f1b82ca0141ee
jv514programming <64076964+jv514programming@users.noreply.github.com>
2020-05-10 12:53:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ setTimeout(function(){
     document.getElementById('richardobackground1').style.backgroundColor = 'blueviolet'
     },5000);
 setTimeout(function(){
-    document.getElementById('richardobackground2').style.backgroundImage = "url('lights.jpg')"
+    document.getElementById('richardobackground2').style.backgroundImage = "url('Lights.jpg')"
     },10000);
     setTimeout(function(){setInterval(() => {
         colorCycle()
